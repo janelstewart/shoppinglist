7161e238a800b71c2199c139226c4780b5915223
Hackbright Student <info@hackbrightacademy.com>
2016-05-03 21:20:19 -0700

    bacon game exercise

---
+++ bacongame.py
@@ -0,0 +1,17 @@
+"""
+A program to determine if you should eat bacon. 
+
+"""
+print "Should you eat that bacon?"
+raw_input("Do you want to feel like angels are frolicking on your tastebuds? Type Yes, No, or Maybe: ")
+if("Yes"):
+    print "eat it!"
+elif("No"):
+    print "You've clearly never tasted bacon. EAT IT!"
+else("Maybe"):
+raw_input("Are you a coward? Type YES or NO")
+    if("YES"):
+        print "Bacon will turn you into a true warrior!"
+    else:
+        print "Then Eat It!"
+#
